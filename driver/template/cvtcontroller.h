3c66647078c1fecdeb1514f3a3d1026979a00f1b
Kim, JoonYong <tombraid@snu.ac.kr>
2018-02-24 12:29:13 +0900

    클래스 구조 작업 시작

--- driver/template/cvtcontroller.h
+++ driver/template/cvtcontroller.h
@@ -1,2 +1,20 @@
-virtual class StdCvtController {
+/**
+ Copyright © 2018 ebio lab. SNU. All Rights Reserved.
+
+ @file cvtcontroller.h
+ @date 2018-02-24, JoonYong
+ @author Kim, JoonYong <tombraid@snu.ac.kr>
+
+ This file is template of CvtController
+ refer from: https://github.com/ebio-snu/stdcvt
+*/
+
+/*
+ @brief CvtController is a virtual class for a smartfarm controller.
+ @date 2018-02-24
+*/
+virtual class CvtController {
+public:
+    CvtController();
+    ~CvtController();
 };
